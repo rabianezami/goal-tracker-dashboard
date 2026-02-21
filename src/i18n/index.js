7662ec54eb4df58c8e379adb76d91ef7eea7e23d
rabia zia nezami <rabianezami786@gail.com>
2026-02-21 23:19:47 +0430

    feat: set up i18n structure with English & Persian translations

---
+++ src/i18n/index.js
@@ -0,0 +1,59 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+
+// English 
+import enCommon from "./locales/en/common.json"
+import enNavigation from "./locales/en/navigation.json"
+import enDashboard from "./locales/en/dashboard.json"
+import enCreateGoal from "./locales/en/createGoal.json"
+import enGoalList from "./locales/en/goalList.json"
+import enGoalDetails from "./locales/en/goalDetails.json"
+import enCategories from "./locales/en/categories.json"
+import enSettings from "./locales/en/settings.json"
+import enNotFound from "./locales/en/notfound.json"
+
+// Farsi 
+import faCommon from "./locales/fa/common.json"
+import faNavigation from "./locales/fa/navigation.json"
+import faDashboard from "./locales/fa/dashboard.json"
+import faCreateGoal from "./locales/fa/createGoal.json"
+import faGoalList from "./locales/fa/goalList.json"
+import faGoalDetails from "./locales/fa/goalDetails.json"
+import faCategories from "./locales/fa/categories.json"
+import faSettings from "./locales/fa/settings.json"
+import faNotFound from "./locales/fa/notfound.json"
+
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                common: enCommon,
+                navigation: enNavigation,
+                dashboard: enDashboard,
+                createGoal: enCreateGoal,
+                goalList: enGoalList,
+                goalDetails: enGoalDetails,
+                categories: enCategories,
+                settings: enSettings,
+                notfound: enNotFound
+            },
+            fa: {
+                common: faCommon,
+                navigation: faNavigation,
+                dashboard: faDashboard,
+                createGoal: faCreateGoal,
+                goalList: faGoalList,
+                goalDetails: faGoalDetails,
+                categories: faCategories,
+                settings: faSettings,
+                notfound: faNotFound
+            }
+        },
+        lng: localStorage.getItem("appLanguage") || "fa",
+        fallbackLng: "fa",
+        interpolation: { escapeValue: false }
+    })
+
+export default i18n
