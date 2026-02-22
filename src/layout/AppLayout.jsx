28ed2aecb81c08f103cf947d3519339c7d5d858c
Baharah Rostami <baharrustami888@gmail.com>
2026-02-22 16:07:36 +0430

    Remove Navbar and Sidebar from AppLayout as per team feedback

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,20 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navbar";
-import SideBar from "../components/sidebar";
 
 export default function AppLayout(){
   return(
     <div className="app-layout">
-      
-      <Navbar/>
-
-      <div className="content-layout">
-        <SideBar/>
-        <div className="page-content">
           <Outlet/>
-        </div>
-      </div>
-
+       
     </div>
   )
 }
