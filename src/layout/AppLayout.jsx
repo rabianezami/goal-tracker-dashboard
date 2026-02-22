36fbdd34abf36b3053ad17fe4caa3d16c0c4906c
Baharah Rostami <baharrustami888@gmail.com>
2026-02-22 13:56:24 +0430

    app layout completed with navbar and sidebar componenets

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,11 +1,20 @@
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/navbar";
+import SideBar from "../components/sidebar";
 
 export default function AppLayout(){
   return(
-      <main>
-        <Outlet/>
-        
-        
-    </main>
+    <div className="app-layout">
+      
+      <Navbar/>
+
+      <div className="content-layout">
+        <SideBar/>
+        <div className="page-content">
+          <Outlet/>
+        </div>
+      </div>
+
+    </div>
   )
 }
