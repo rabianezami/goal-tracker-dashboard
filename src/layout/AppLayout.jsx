09ca16f5720e97199c736bc1399bf76e4d205692
fataneh-mow <fataneh.mow@gmail.com>
2026-02-24 00:44:50 +0430

    feat: implemented Navbar

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
-
+import Navbar from "../components/Navbar";
 export default function AppLayout(){
   return(
+    <div>
+      <Navbar progress={"0%"}></Navbar>
       <main>
-        <Outlet/>
-    </main>
+          <Outlet/>
+      </main>
+    </div>
+      
   )
 }
