c811ea936e873791620e83186d6056d58b6ef213
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-02-24 22:49:21 +0430

    feat: add new components for goalDetails page

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,12 +1,12 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 export default function AppLayout(){
   return(
     <Box sx={{ display: "flex", flexDirection: "row-reverse"}}>
-        <Navbar/>
+        {/* <Navbar/> */}
         <Sidebar/>
          <Box component="main">
           <Outlet/>
