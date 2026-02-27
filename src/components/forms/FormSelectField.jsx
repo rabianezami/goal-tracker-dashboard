135c7ddb2c879b709d94cc9c6690e4108f8ad08a
rabia zia nezami <rabianezami786@gail.com>
2026-02-27 23:01:11 +0430

    feat: add dynamic icons

--- src/components/forms/FormSelectField.jsx
+++ src/components/forms/FormSelectField.jsx
@@ -1,3 +1,4 @@
+import { InputAdornment } from "@mui/material"
 import MenuItem from "@mui/material/MenuItem"
 import TextField from "@mui/material/TextField"
 import { Controller } from "react-hook-form"
@@ -5,6 +6,7 @@ import { Controller } from "react-hook-form"
 const FormSelectField = ({
     name,
     control,
+    icon,
     options = [],
     ...props
 }) => {
@@ -20,6 +22,15 @@ const FormSelectField = ({
                     fullWidth
                     error={!!fieldState.error}
                     helperText={fieldState.error?.message}
+                    slotProps={{
+                        input: {
+                           startAdornment: icon ? (
+                            <InputAdornment position="start">
+                               {icon}
+                            </InputAdornment>
+                           ) : undefined
+                        }
+                    }}
                 >
                     {options.map((option) => (
                         <MenuItem key={option.value} value={option.value}>
--- src/components/forms/FormTextField.jsx
+++ src/components/forms/FormTextField.jsx
@@ -1,28 +1,43 @@
-import TextField from "@mui/material/TextField"
 import { Controller } from "react-hook-form"
+import { TextField, InputAdornment } from "@mui/material"
 
 const FormTextField = ({
-    name,
-    control,
-    slotProps,
-    ...props
+  name,
+  control,
+  label,
+  icon,
+  helperText,
+  ...props
 }) => {
-    return (
-        <Controller
-            name={name}
-            control={control}
-            render={({ field, fieldState }) => (
-                <TextField
-                    {...field}
-                    {...props}
-                    fullWidth
-                    slotProps={slotProps}
-                    error={!!fieldState.error}
-                    helperText={fieldState.error?.message}
-                />
-            )}
-        />
-    )
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => {
+        const hasError = !!fieldState.error;
+
+        return (
+          <TextField
+            {...field}
+            {...props}
+            fullWidth
+            label={label}
+            error={hasError}
+            helperText={hasError ? fieldState.error.message : helperText}
+            slotProps={{
+              input: {
+                startAdornment: icon ? (
+                  <InputAdornment position="start">
+                    {icon}
+                  </InputAdornment>
+                ) : undefined,
+              }
+            }}
+          />
+        )
+      }}
+    />
+  )
 }
 
-export default FormTextField 
+export default FormTextField
