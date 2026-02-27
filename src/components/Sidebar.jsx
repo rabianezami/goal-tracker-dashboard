efac91ea85ae80d787cba9ce89a8f5f406906b97
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-02-27 22:57:03 +0430

    feat: enhance Sidebar component with responsive design and archive section

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,115 +1,126 @@
 import {
-  Drawer,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Badge,
   Box,
+  ListItemButton,
+  ListItemText,
   Typography,
+  List,
   Divider,
+  ListItemIcon,
+  Badge,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
 } from "@mui/material";
-import { NavLink } from "react-router-dom";
-import { navItems } from "../data/navigation";
-import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-const drawerWidth = 250;
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { NavLink } from "react-router-dom";
+import { navItems, archivedGoals } from "../data/navigation";
 
 export default function Sidebar() {
   return (
-    <Drawer
-      variant="permanent"
-      anchor="left"
+    <Box
       sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: drawerWidth,
-          boxSizing: "border-box",
-          position: "relative",
-        },
+        width: 230,
+        minHeight: "calc(100vh - 64px)",
+        borderRight: "1px solid",
+        borderColor: "divider",
+        p: 2,
+        display: { xs: "none", md: "block" }, // responsive
       }}
     >
-      <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
-        <List>
-          {navItems.map((item) => (
-            <ListItem key={item.path} disablePadding>
-              <ListItemButton
-                component={NavLink}
-                to={item.path}
-                sx={{
-                  "&.active": {
-                    bgcolor: "primary.light",
-                    color: "primary.main",
-                    fontWeight: 600,
-                  },
-                }}
-              >
-                <ListItemText
-                  primary={item.label}
-                  sx={{ marginLeft: 2.5 }}
-                />
+      {/* Navigation Title */}
+      <Typography
+        variant="subtitle2"
+        sx={{ mb: 1, color: "text.secondary" }}
+      >
+        NAVIGATION
+      </Typography>
 
-                <ListItemIcon sx={{ minWidth: 36, marginRight: 0 }}>
-                  {item.badge ? (
-                    <Badge badgeContent={item.badge} color="primary">
-                      {item.icon}
-                    </Badge>
-                  ) : (
-                    item.icon
-                  )}
-                </ListItemIcon>
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+      {/* Main Navigation */}
+      <List>
+        {navItems.map((item) => (
+          <ListItemButton
+            key={item.path}
+            component={NavLink}
+            to={item.path}
+            sx={{
+              "&.active": {
+                bgcolor: "action.selected",
+              },
+            }}
+          >
+            <ListItemIcon sx={{ minWidth: 55 }}>
+              {item.badge ? (
+                <Badge badgeContent={item.badge} color="primary">
+                  {item.icon}
+                </Badge>
+              ) : (
+                item.icon
+              )}
+            </ListItemIcon>
 
-        <Divider />
+            <ListItemText primary={item.label} />
+          </ListItemButton>
+        ))}
+      </List>
 
-        {/* Archive */}
-        <Box sx={{ p: 1 }}>
-          <Accordion>
+      <Divider sx={{ my: 2 }} />
+
+      {/* Archive Section */}
+      <Accordion disableGutters elevation={0}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography variant="subtitle1">
+            اهداف آرشیو شده
+          </Typography>
+        </AccordionSummary>
+
+        <AccordionDetails sx={{ p: 0 }}>
+
+          {/* Incomplete */}
+          <Accordion disableGutters elevation={0}>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography fontWeight={600}>اهداف آرشیو شده</Typography>
+              <Typography variant="body2">
+                اهداف کامل نشده
+              </Typography>
             </AccordionSummary>
-
-            <AccordionDetails sx={{ p: 0 }}>
-              {/* Incomplete */}
-              <Accordion disableGutters elevation={0}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                  <Typography>اهداف کامل نشده</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <List dense>
-                    <ListItemButton>
-                      <ListItemText primary="مطالعه کتاب" />
-                    </ListItemButton>
-                    <ListItemButton>
-                      <ListItemText primary="ورزش" />
-                    </ListItemButton>
-                  </List>
-                </AccordionDetails>
-              </Accordion>
-
-              {/* Complete */}
-              <Accordion disableGutters elevation={0}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                  <Typography>اهداف کامل شده</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <List dense>
-                    <ListItemButton>
-                      <ListItemText primary="مدیتیشن روزانه" />
-                    </ListItemButton>
-                  </List>
-                </AccordionDetails>
-              </Accordion>
+            <AccordionDetails>
+              <List dense>
+                {archivedGoals.incomplete.map((goal) => (
+                  <ListItemButton key={goal.id}>
+                    <ListItemText primary={goal.title} />
+                  </ListItemButton>
+                ))}
+              </List>
             </AccordionDetails>
           </Accordion>
-        </Box>
-      </Box>
-    </Drawer>
+
+          {/* Complete */}
+          <Accordion disableGutters elevation={0}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="body2">
+                اهداف کامل شده
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <List dense>
+                {archivedGoals.complete.map((goal) => (
+                  <ListItemButton key={goal.id}>
+                    <ListItemText primary={goal.title} />
+                  </ListItemButton>
+                ))}
+              </List>
+            </AccordionDetails>
+          </Accordion>
+
+        </AccordionDetails>
+      </Accordion>
+
+      <Divider sx={{ my: 2 }} />
+
+      {/* Footer */}
+      <Typography variant="body2" color="text.secondary">
+        Logged in as: <b>admin</b>
+      </Typography>
+    </Box>
   );
 }
