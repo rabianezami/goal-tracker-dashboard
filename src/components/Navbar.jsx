05ac84c68987fdf21beee0e7958a4605df9cfb15
fataneh-mow <fataneh.mow@gmail.com>
2026-03-01 00:07:14 +0430

    fix: changed navbar structure to use mui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,93 @@
-// import { Box, Typography } from '@mui/material';
-import  user from "../assets/user-image.webp";
+import { Box, Typography, Avatar, Paper } from "@mui/material";
+import user from "../assets/user-image.webp";
+import bgImage from "../assets/bg.webp"
 
-export default function Navbar({progress}) {
+export default function Navbar({ progress, userName }) {
   return (
-    <div>
-      <div className="p-4 background-container shadow-lg">
-        <div className="flex justify-between items-center">
+    <Box
+      sx={(theme) => ({
+        width: "100%",
+        mx: 0,
+        py: 2,
+        boxShadow: 3,
+        height: "14vh",
+        backgroundImage:
+          theme.palette.mode === "dark"
+            ? `
+              linear-gradient(
+                rgba(0,0,0,0.25),
+                rgba(255,255,225,0.1)
+              ),
+              url(${bgImage})
+            `
+            : `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      })}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          elevation={4}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 3,
+            px: 3,
+            backgroundColor: "transparent",
+          }}
+        >
+          <Avatar
+            src={user}
+            alt="User"
+            sx={{
+              width: 70,
+              height: 70,
+              boxShadow: 3,
+            }}
+          />
 
-          <div className="flex items-center gap-4 bg-gray-50/60 rounded-md px-4 mx-12 shadow-md">
-            <img src={user} 
-              alt="user-image"
-              className="w-20 h-20 shadow-md rounded-full relative right-8"
-            />
-            <div className="text-center mx-12">
-              <p className="mx-8 font-semibold text-blue-900 text-2xl">User's Name</p>
-              <p className="py-2 text-gray-600 text-sm">{progress || "72 % completed!"}</p>
-            </div>
-          </div>
+          <Box>
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: 600,
+                color: "primary.dark",
+              }}
+            >
+              {userName || "User's Name"}
+            </Typography>
 
-          <div className="text-right font-bold text-[4rem] text-blue-950 mx-12">
-            My Goal Tracker
-          </div>
+            <Typography
+              variant="body2"
+              sx={{
+                color: "text.secondary",
+                mt: 0.5,
+              }}
+            >
+              {progress || "72% completed!"}
+            </Typography>
+          </Box>
+        </Box>
 
-        </div>
-      </div>
-    </div>
+        <Typography
+          variant="h3"
+          sx={{
+            fontWeight: 800,
+            color: "primary.dark",
+            letterSpacing: 1,
+            textShadow: "2px 2px 6px rgba(225,225,225,10)",
+          }}
+        >
+          My Goal Tracker
+        </Typography>
+      </Box>
+    </Box>
   );
 }
