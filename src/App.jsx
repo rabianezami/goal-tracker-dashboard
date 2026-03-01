3caee43e91f6b0f3db9dcd84e1f63f9bd9ed4bdc
rabia zia nezami <rabianezami786@gmail.com>
2026-03-01 04:41:27 +0430

    cssbaseline remove beacause it's already exist in AppThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState} from "react"
 import AppThemeProvider from "./providers/AppThemeProvider"
 import AppRoutes from "./routes/AppRoutes"
 import "./index.css"
-import {CssBaseline} from "@mui/material";
 import "./i18n/index.js"
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
      <AppThemeProvider>
-        <CssBaseline></CssBaseline>
         <AppRoutes />
      </AppThemeProvider>
     </>
