624ddd5a3429764e6f0263573c2afe563879f3ca
fataneh-mow <fataneh.mow@gmail.com>
2026-03-03 11:26:23 +0430

    feat: created goalForm schema for goalForm

---
+++ src/validations/goalSchema.js
@@ -0,0 +1,62 @@
+import * as yup from "yup";
+
+export const goalSchema = yup.object({
+    title: yup
+        .string()
+        .trim()
+        .required("Title is required")
+        .min(3, "Title must be at least 3 characters")
+        .max(100, "Title must not be more that 100 characters")
+    ,
+    
+    goalCategory: yup
+        .string()
+        .required("Category is reqiured")
+        .oneOf(["Health", "Study", "Work", "Personal"], "Invalid category")
+    ,
+
+    goalType: yup
+        .string()
+        .required("Type is required")
+        .oneOf(["Daily", "Count-based", "Time-based"], "Invalid goal type")
+    ,
+
+    target: yup
+        .number()
+        .typeError("Please choose a number")
+        .positive("Please choose a positive number")
+        .required("target is required")
+    ,
+
+    startDate: yup
+        .date()
+        .required("Start date is required")
+    ,
+
+    endDate: yup 
+        .date()
+        .nullable()
+        .notRequired()
+        .min(
+            yup.ref("startDate"), "End date must be after start date"
+        )
+    ,
+
+    color: yup
+        .string()
+        .trim()
+        .notRequired()
+    ,
+
+    icon: yup
+        .string()
+        .trim()
+        .notRequired()
+    ,
+
+    description: yup
+        .string()
+        .trim()
+        .notRequired()
+        .max(250, "Description maximum is 250 characters")
+})
