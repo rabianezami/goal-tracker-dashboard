e7ddfbf42424c1257fbc823b0931292a24a6f348
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-03-16 23:10:33 +0430

    feat: add reusable DashboardHeader component and update sidebar navigation paths

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,12 +1,16 @@
 import { useGoals } from "../context/GoalsContext"
 import GoalList from "../components/GoalList"
+import DashboardHeader from "../components/dashboard/DashboardHeader"
+import { Container } from "@mui/material"
 
 export default function Dashboard() {
 
   const { goals, removeGoal } = useGoals()
 
   return (
-    <div>
+    <Container maxWidth="lg">
+      <DashboardHeader />
+      <div>
       <GoalList
         goals={goals}
         onEdit={() => {}}
@@ -14,6 +18,7 @@ export default function Dashboard() {
         onToggleStatus={() => {}}
         onOpenDetails={() => {}}
       />
-    </div>
+      </div>
+    </Container>
   )
 }
