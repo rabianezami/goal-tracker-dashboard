aac3659be20df9bef45966ab8a4f8c83f562d910
fataneh-mow <fataneh.mow@gmail.com>
2026-03-20 19:50:06 +0430

    feat: created the pages for all categories avaliable in the project

---
+++ src/pages/category-based-pages/Personal.jsx
@@ -0,0 +1,79 @@
+import { Box, Container, Typography, Divider } from "@mui/material"
+import { useTranslation } from "react-i18next"
+import CategoryCard from "../../components/categories/CategoryCard";
+import Chart from "../../components/categories/CategoryPageChart"
+
+export default function Personal() {
+    const { t } = useTranslation("categories")
+
+    const goals = (JSON.parse(localStorage.getItem("goals")) || [])
+        .filter(goal => goal.goalCategory === "personal");
+    const categoryStats = {};
+
+    goals.forEach((goal) => {
+        const category = goal.goalCategory;
+
+        if (!categoryStats[category]) {
+            categoryStats[category] = {
+                title: category,
+                total: 0,
+                active: 0,
+                completed: 0
+            };
+        }
+
+        categoryStats[category].total += 1;
+
+        if (goal.status === "active") {
+            categoryStats[category].active += 1;
+        }
+
+        if (goal.status === "completed") {
+            categoryStats[category].completed += 1;
+        }
+    });
+
+    const categories = Object.values(categoryStats);
+
+    return (
+        <Container>
+            <Box sx={{
+                my: 2,
+            }}>
+                <Typography variant="h6">{t("titleCat", {cat: goals[0]?.goalCategory || "Personal"})}</Typography>
+            </Box>
+            <Divider></Divider>
+            <Box
+                sx={{
+                    display: "grid",
+                    gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
+                    columnGap: 2,
+                    my: 4
+                }}
+            >
+                {
+                    categories.map((category) => (
+                        <Box
+                            key={category.title}
+                        >
+                            <CategoryCard
+                                title={category.title}
+                                total={category.total}
+                                active={category.active}
+                                completed={category.completed}
+                            />
+                        </Box>
+
+                    ))
+                }
+            </Box>
+            <Box
+                sx={{
+                    mx: 0
+                }}
+            >
+                <Chart goals={goals}></Chart>
+            </Box>
+        </Container>
+    )
+}
