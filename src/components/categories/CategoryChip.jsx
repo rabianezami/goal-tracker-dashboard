b88f27eaa0f89d5921dff94caa6c72e82c2a97f5
fataneh-mow <fataneh.mow@gmail.com>
2026-03-20 19:47:18 +0430

    feat: created a chip component button for navigating to different category pages

---
+++ src/components/categories/CategoryChip.jsx
@@ -0,0 +1,26 @@
+import { Box, Button } from "@mui/material"
+import { useNavigate } from "react-router"
+
+export default function CategoryChip ({pathTo, categoryName}) {
+      const navigate = useNavigate()
+
+    return (
+        <Box>
+            <Button 
+                onClick={() => navigate(pathTo)}
+                sx={{
+                    background: "#f59e0b",
+                    color: "#ffffff",
+                    px: 4,
+                    "&:hover": {
+                        backgroundColor: "#e8940d",
+                        cursor: "pointer",
+                    },
+                    transition: "all 200ms ease-in-out",
+                }}
+            >
+                {categoryName}
+            </Button>
+        </Box>
+    )
+}
