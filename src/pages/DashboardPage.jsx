e6d7e66ed06e6bccc8117f1062bb80eed82b34e0
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-25 23:14:31 +0430

    fix: simplify dashboard structure and remove duplicate return

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,33 +1,18 @@
-import { useGoals } from "../context/GoalsContext"
-import GoalList from "../components/GoalList"
-import CustomeButton from "../components/dashboard/CustomeButton"
-import { Container } from "@mui/material"
+import { Container, Stack } from "@mui/material"
+import DashboardSummary from "../components/dashboard/DashboardSummary"
+import QuickActions from "../components/dashboard/QuickActions"
+import ActiveGoalsList from "../components/dashboard/ActiveGoalLists"
+import CompletedGoalsPreview from "../components/dashboard/CompletedGoalsPreview"
 
 export default function Dashboard() {
   return (
-   <Box p={2}>
-      <DashBoardSummary />
-      
-      <Box mt={2}>
-        <QuickActions />
-      </Box>
-
-      <Box mt={3}>
-        <ActiveGoalsList />
-      </Box>
-
-  return (
     <Container maxWidth="lg">
-      <CustomeButton />
-      <div>
-      <GoalList
-        goals={goals}
-        onEdit={() => {}}
-        onDelete={removeGoal}
-        onToggleStatus={() => {}}
-        onOpenDetails={() => {}}
-      />
-      </div>
+      <Stack spacing={3}>
+        <DashboardSummary />
+        <QuickActions />
+        <ActiveGoalsList />
+        <CompletedGoalsPreview />
+      </Stack>
     </Container>
   )
 }
