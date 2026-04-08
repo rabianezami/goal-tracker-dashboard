d20322bcd92d785b0a4a448578ab06d1cbbc62a3
Baharah Rostami <baharrustami888@gmail.com>
2026-04-08 21:30:51 +0430

    fixed the auth context by making it safety, default values and updated login function

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,24 +1,35 @@
 import { createContext, useContext, useState } from "react";
 
-export const AuthContext = createContext();
+export const AuthContext = createContext({
+  user: null,
+  isLoggedIn: false,
+  login: () => {},
+  logout: () => {},
+});
 
 export default function AuthProvider({children}) {
     const [user, setUser] = useState(null);
 
     const isLoggedIn = !!user;
 
-    const login = (userData)=>{
-        setUser(userData)
-    }
+   const login = ({ name, email }) => {
+   setUser({ name, email });
+    };
+
     const logout = ()=>{
         setUser(null)
-    }
+    };
   return (
    <AuthContext.Provider value={{user, isLoggedIn, login, logout}}>
     {children}
    </AuthContext.Provider>
   )
 }
+
 export function useAuth() {
-    return useContext(AuthContext)
+    const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
 }
