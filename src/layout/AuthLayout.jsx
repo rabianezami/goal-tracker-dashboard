1c64e2a1070e9c45677e781b11e84c5a8c59f5da
rabia zia nezami <rabianezami786@gmail.com>
2026-04-08 22:59:37 +0430

    feat: add AuthLayout for login and register

---
+++ src/layout/AuthLayout.jsx
@@ -0,0 +1,28 @@
+import { Box, Paper } from "@mui/material";
+
+export default function AuthLayout({ children }) {
+    return (
+        <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "background.default",
+                px: 2
+            }}
+        >
+            <Paper
+                elevation={3}
+                sx={{
+                    width: "100%",
+                    maxWidth: 400,
+                    p: 4,
+                }}
+            >
+                {children}
+            </Paper>
+        </Box>
+    )
+
+}
