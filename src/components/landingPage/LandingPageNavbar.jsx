e8f6408ddf9d4b5935c920d53cb876bc5b16895d
fataneh-mow <fataneh.mow@gmail.com>
2026-04-21 19:43:28 +0430

    navbar for landing page created

--- src/components/landingPage/LandingPageNavbar.jsx
+++ src/components/landingPage/LandingPageNavbar.jsx
@@ -1,7 +1,54 @@
-import { Box, Typography, Paper } from "@mui/material";
+import { Box, Typography, AppBar, Toolbar, Button, Divider } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-export default function LandingPageNavbar () {
+export default function LandingPageNavbar() {
+    const {t} = useTranslation("landingPage");
+
+    const linkStyle = ({ isActive }) => ({
+        textDecoration: "none",
+        color: isActive ? "#1976d2" : "inherit",
+    });
+
     return (
-        
-    )
+        <Box sx={{
+            position: "static",
+        }}>
+            <Toolbar sx={{
+                display: "flex", 
+                justifyContent: "space-between",
+                pt: 2,
+            }}>
+
+                {/* Links */}
+                <Box sx={{ display: "flex", gap: 2 }}>
+                    <Button component={NavLink} to="/home" style={linkStyle}>
+                        {t("navbar.HOME")}
+                    </Button>
+
+                    <Button component={NavLink} to="/login" style={linkStyle}>
+                        {t("navbar.LOGIN")}
+                    </Button>
+
+                    <Button component={NavLink} to="/signup" style={linkStyle}>
+                        {t("navbar.SIGNUP")}
+                    </Button>
+                </Box>
+
+            </Toolbar>
+            <Divider
+                sx={{
+                    borderColor: "gray",
+                    mr: 24,
+                    my: 1,
+                }}
+            />
+            <Divider
+                sx={{
+                    borderColor: "gray",
+                    ml: 24
+                }}
+            />
+        </Box>
+    );
 }
